8b1fc91d6d7441a871848131de1ad0afaaed93be
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-03-10 22:18:34 +0300

    Добавлены model-класс Album, Post, Todo

---
+++ src/main/java/com/example/vkrestapi/models/Post.java
@@ -0,0 +1,11 @@
+package com.example.vkrestapi.models;
+
+import lombok.Data;
+
+@Data
+public class Post {
+    private Integer userId;
+    private Integer id;
+    private String title;
+    private String body;
+}
