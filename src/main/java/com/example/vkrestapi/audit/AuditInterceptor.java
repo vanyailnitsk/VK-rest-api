1837167d149b11c79684ddb789f96f7828398182
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-03-12 21:12:29 +0300

    Удалены ненужные импорты

--- src/main/java/com/example/vkrestapi/audit/AuditInterceptor.java
+++ src/main/java/com/example/vkrestapi/audit/AuditInterceptor.java
@@ -8,12 +8,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
@@ -22,7 +19,7 @@ import java.util.Set;
 @Slf4j
 public class AuditInterceptor implements HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
         String method = request.getMethod();
@@ -57,12 +54,12 @@ public class AuditInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(
             HttpServletRequest request, HttpServletResponse response, Object handler,
-            ModelAndView modelAndView) throws Exception {
+            ModelAndView modelAndView){
 
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
-                                Object handler, Exception exception) throws Exception {
+                                Object handler, Exception exception) {
     }
 }
--- src/main/java/com/example/vkrestapi/audit/WebMvcInterceptor.java
+++ src/main/java/com/example/vkrestapi/audit/WebMvcInterceptor.java
@@ -1,15 +1,8 @@
 package com.example.vkrestapi.audit;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
-import org.springframework.web.filter.ServletContextRequestLoggingFilter;
-import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
