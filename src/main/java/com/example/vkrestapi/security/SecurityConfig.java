c25092b54b0ce518c347f8ed989e1e183e7a9c63
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-03-13 00:09:37 +0300

    UserDetailsServiceImpl внедрение по конструктору

--- src/main/java/com/example/vkrestapi/security/SecurityConfig.java
+++ src/main/java/com/example/vkrestapi/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.vkrestapi.security;
 
+import com.example.vkrestapi.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +12,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,33 +26,34 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 @EnableMethodSecurity
 public class SecurityConfig {
+    private final UserDetailsServiceImpl userDetailsService;
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-//        PasswordEncoder passwordEncoder = passwordEncoder();
-//        UserDetails postRole = UserDetailsImpl.builder()
-//                .username("post")
-//                .password(passwordEncoder.encode("post"))
-//                .roles(Set.of(Role.ROLE_POSTS))
-//                .build();
-//        UserDetails albumRole = UserDetailsImpl.builder()
-//                .username("album")
-//                .password(passwordEncoder.encode("album"))
-//                .roles(Set.of(Role.ROLE_ALBUMS))
-//                .build();
-//        UserDetails userRole = UserDetailsImpl.builder()
-//                .username("user")
-//                .password(passwordEncoder.encode("user"))
-//                .roles(Set.of(Role.ROLE_USERS))
-//                .build();
-//        UserDetails adminRole = UserDetailsImpl.builder()
-//                .username("admin")
-//                .password(passwordEncoder.encode("admin"))
-//                .roles(Set.of(Role.ROLE_ADMIN))
-//                .build();
-//        return new InMemoryUserDetailsManager(postRole,albumRole,userRole,adminRole);
-        return new UserDetailsServiceImpl();
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+////        PasswordEncoder passwordEncoder = passwordEncoder();
+////        UserDetails postRole = UserDetailsImpl.builder()
+////                .username("post")
+////                .password(passwordEncoder.encode("post"))
+////                .roles(Set.of(Role.ROLE_POSTS))
+////                .build();
+////        UserDetails albumRole = UserDetailsImpl.builder()
+////                .username("album")
+////                .password(passwordEncoder.encode("album"))
+////                .roles(Set.of(Role.ROLE_ALBUMS))
+////                .build();
+////        UserDetails userRole = UserDetailsImpl.builder()
+////                .username("user")
+////                .password(passwordEncoder.encode("user"))
+////                .roles(Set.of(Role.ROLE_USERS))
+////                .build();
+////        UserDetails adminRole = UserDetailsImpl.builder()
+////                .username("admin")
+////                .password(passwordEncoder.encode("admin"))
+////                .roles(Set.of(Role.ROLE_ADMIN))
+////                .build();
+////        return new InMemoryUserDetailsManager(postRole,albumRole,userRole,adminRole);
+//        return new UserDetailsServiceImpl();
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -73,7 +74,7 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setUserDetailsService(userDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
     }
--- src/main/java/com/example/vkrestapi/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/vkrestapi/service/UserDetailsServiceImpl.java
@@ -1,18 +1,19 @@
-package com.example.vkrestapi.security;
+package com.example.vkrestapi.service;
 
 import com.example.vkrestapi.repository.UserDetailsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.vkrestapi.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Component
+@Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
-    @Autowired
-    private UserDetailsRepository repository;
+    private final UserDetailsRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
