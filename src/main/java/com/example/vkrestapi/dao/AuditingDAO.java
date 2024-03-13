2b684b41675ec0bf0bd2cda394808a1dc2e2e231
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-03-13 11:02:43 +0300

    Entity для аудита действий

--- src/main/java/com/example/vkrestapi/dao/AuditingDAO.java
+++ src/main/java/com/example/vkrestapi/dao/AuditingDAO.java
@@ -1,4 +1,26 @@
 package com.example.vkrestapi.dao;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "auditing")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AuditingDAO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String userRoles;
+    private String uri;
+    private String method;
+    private Boolean hasAccess;
+    private String param;
+
 }
